882731d0e00bcdbd311f679cf7db53f52ac0a564
ariajahan <ariajahan.work@gmail.com>
2025-09-09 11:56:33 +0600

    Add files via upload

---
+++ BEE-1046.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+int start, end, duration;
+scanf("%d %d", &start, &end);
+
+if (start < end)
+        {
+duration = end - start;
+}
+else
+{
+duration = 24 - start + end; // Game ended next day
+}
+printf("O JOGO DUROU %d HORA(S)\n", duration);
+return 0;
+}
